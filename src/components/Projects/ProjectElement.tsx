85ed8b12e504c2e19fa4131656decb0397d7f523
Jyrki Oraskari <31693668+jyrkioraskari@users.noreply.github.com>
2022-03-24 17:50:15 +0100

    TypeScript

--- src/components/Projects/ProjectElement.tsx
+++ src/components/Projects/ProjectElement.tsx
@@ -13,21 +13,23 @@ import PubSub from "pubsub-js";
 import { ReactSession } from "react-client-session";
 import { useNavigate } from "react-router-dom";
 
-export default function ProjectElement({
-  project: { projectName, projectId },
-  keyvalue,
-  history,
-}) {
+type ProjectElement = {
+  project: { projectName: string; projectId: string };
+  keyvalue: string;
+  history: any;
+};
+
+export default function ProjectElement(props: ProjectElement) {
   const navigate = useNavigate();
 
   function handleClick() {
-    ReactSession.set("projectid", projectId);
-    PubSub.publish("ProjectName", { name: projectName });
-    PubSub.publish("SidebarName", { name: projectName });
-    navigate(projectName + "/");
+    ReactSession.set("projectid", props.project.projectId);
+    PubSub.publish("ProjectName", { name: props.project.projectName });
+    PubSub.publish("SidebarName", { name: props.project.projectName });
+    navigate(props.project.projectName + "/");
   }
   return (
-    <div style={{ width: 340, margin: "auto", align_items: "center" }}>
+    <div style={{ width: 340, margin: "auto", alignItems: "center" }}>
       <Card withBorder={true} color={"blue"} shadow="sm" p="md">
         <Card.Section></Card.Section>
 
@@ -35,7 +37,7 @@ export default function ProjectElement({
           position="apart"
           style={{ marginBottom: 5 /*marginTop: theme.spacing.sm*/ }}
         >
-          <Text weight={700}>{projectName}</Text>
+          <Text weight={700}>{props.project.projectName}</Text>
         </Group>
         <RingProgress
           label={
@@ -52,8 +54,12 @@ export default function ProjectElement({
         <Text size="sm" style={{ lineHeight: 1.5 }}>
           lorem ipsum dolor sit
         </Text>
-        <Button value={projectId} key={String(keyvalue)} onClick={handleClick}>
-          Open {projectName}
+        <Button
+          value={props.project.projectId}
+          key={String(props.keyvalue)}
+          onClick={handleClick}
+        >
+          Open {props.project.projectName}
         </Button>
       </Card>
     </div>
