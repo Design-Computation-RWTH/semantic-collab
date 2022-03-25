888cdee6718de4f39bbfada567dcf0f0378bf5e6
Jyrki Oraskari <31693668+jyrkioraskari@users.noreply.github.com>
2022-03-25 17:37:01 +0100

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,6 @@
 import Treeview from "./pages/Treeview";
+import SetupView from "./pages/SetupsView";
+import AddUserForm from "./components/Setup/AddUserForm";
 
 var enzyme = require("enzyme");
 
@@ -6,4 +8,11 @@ var wrapper = enzyme.shallow(<Treeview />);
 
 it("renders without crashing", () => {
   enzyme.shallow(<Treeview />);
+});
+
+it("renders Account header", () => {
+  const wrapper = enzyme.shallow(<AddUserForm onHide={() => {}} show="true" />);
+  const email_greeting =
+    "The e-mail address of an existing user at the system.";
+  expect(wrapper.contains(email_greeting)).toEqual(true);
 });
