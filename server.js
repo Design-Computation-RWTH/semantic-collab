ef1fab194ba0a60618d67073317ef45c12e169e8
Oliver Schulz <9889671+OlliSchu@users.noreply.github.com>
2022-04-04 14:00:27 +0200

    Fixed server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // express.static.mime.define({'application/wasm': ['wasm']});
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', function (req, res) {
  res.sendFile(path.join(__dirname, 'build', 'index.html'));
